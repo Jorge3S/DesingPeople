930f4b60899c129a8f30dc3031d9334be117f723
Jorge3S <124838059+Jorge3S@users.noreply.github.com>
2023-03-09 16:17:29 -0600

    Actualizacion

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/components/HelloWorld.vue",
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
